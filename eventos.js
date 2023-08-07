1f0ffe2a65e0fbb630a4bfa8ae6fa2aeb4f6e969
federico-paganini <perfedefer@gmail.com>
2023-08-07 19:51:58 -0300

    Alerta div agregada
    Se crea el JavaScript eventos, se agrega al html, div agregado con su respectivo estilo.

---
+++ eventos.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const divselect = document.querySelector("div");
+    
+    divselect.addEventListener("click", function() {
+    alert('Hola! Soy el div');
+    });
+
+    
+});
