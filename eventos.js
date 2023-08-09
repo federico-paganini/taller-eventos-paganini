dfff6d8f5ec71205df1abc0ef0dafb18b5bc0d39
federico-paganini <perfedefer@gmail.com>
2023-08-08 21:07:29 -0300

    Solución propagación clic.
    Se cambia el evento para el js y se detiene la propagacion del clic al div.

--- eventos.js
+++ eventos.js
@@ -1,9 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     const divselect = document.querySelector("div");
+    const btn = document.querySelector("button");
     
     divselect.addEventListener("click", function() {
     alert('Hola! Soy el div');
     });
 
-    
+    btn.addEventListener("click", function() {
+        event.stopPropagation()
+        alert('Hola!');
+    });
 });
